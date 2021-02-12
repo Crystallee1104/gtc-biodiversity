98c91e6db70774d3ddc7d9025e4acfd52768cf82
Simon Thomas <sdat2@cam.ac.uk>
2021-02-12 15:56:02 +0000

    finished exploratory branch

--- src/preprocessing/rast_to_poly.py
+++ src/preprocessing/rast_to_poly.py
@@ -3,33 +3,49 @@ Adding examples of raster to polygon functions (probably not working that well).
 """
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 import src.constants as cst
 import rasterio
-import matplotlib.pyplot as plt
-from rasterio import features
 import geopandas as gpd
+from rasterio import features
 from rasterio.features import shapes
 from rasterio import plot as rplot
 from rasterio.plot import show as rshow
 from rasterio.mask import mask as rmask
 
 
-def arcpy_nonsimplified(workspace, outPolygons, inRaster="zone", field="VALUE"):
+def raster_to_poly(input_tif_path, output_geojson_path):
     """
-    https://desktop.arcgis.com/en/arcmap/10.3/tools/conversion-toolbox/raster-to-polygon.htm
-    # conda install -c esri arcgis
-    # pip install arcgis 
-    :param workspace: path e.g. "C:/data"
-    :param workspace: file path e.g. "c:/output/zones.shp"
-    :param inRaster:
-    :param field:
+    Using rasterio to go from raster to polygon
+    using a tif file as an input.
+    input_tif_path: full path and name of input tif file.
+    output_geojson_path: full pathe and name of output geojson file.
     """
-    import arcpy
-    from arcpy import env
-    # Set environment settings
-    env.workspace = workspace
-    # Execute RasterToPolygon
-    arcpy.RasterToPolygon_conversion(inRaster, outPolygons, "NO_SIMPLIFY", field)
+    mask = None
+    with rasterio.Env():
+        with rasterio.open(input_tif_path) as image:
+            image_band_1 = image.read(1) # first band
+            results = (
+            {'properties': {'raster_val': v}, 'geometry': s}
+            for i, (s, v) 
+            in enumerate(
+                shapes(image_band_1, mask=mask, transform=image.transform)))
+    geoms = list(results)
+    gpd_polygonized_raster = gpd.GeoDataFrame.from_features(geoms)
+    gpd_polygonized_raster.to_file(output_geojson_path, driver='GeoJSON')
+
+
+def esa_cci_to_geojson(output_dir=os.path.join(os.path.dirname(os.path.realpath(__file__)), "out")):
+    """
+    output_dir: path to folder with no trailing slash
+
+    """
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    for year in range(1992, 2016):
+        path = "/gws/nopw/j04/ai4er/guided-team-challenge/2021/biodiversity/esa_cci_rois/esa_cci_" + str(year) + "_chernobyl.tif"
+        raster_to_poly(path, os.path.join(output_dir, "esa_cci_" + str(year) + "_chernobyl.geojson"))
 
 
 def test_rast():
@@ -79,28 +95,6 @@ def test_rast():
     print(df_places.loc[1].geometry)
 
 
-def raster_to_poly(input_tiff_path, output_geojson_path):
-    """
-    Using rasterio to go from raster to polygon
-    but needs a tiff with an input.
-    usage: 
-
-    """
-    mask = None
-    with rasterio.Env():
-        with rasterio.open(input_tiff_path) as image:
-            image_band_1 = image.read(1) # first band
-            results = (
-            {'properties': {'raster_val': v}, 'geometry': s}
-            for i, (s, v) 
-            in enumerate(
-                shapes(image_band_1, mask=mask, transform=image.transform)))
-    geoms = list(results)
-    gpd_polygonized_raster = gpd.GeoDataFrame.from_features(geoms)
-    print(gpd_polygonized_raster)
-    gpd_polygonized_raster.to_file(output_geojson_path, driver='GeoJSON')
-
-
 def trial_esa_cci():
     for year in range(1992, 2016):
         path = "/gws/nopw/j04/ai4er/guided-team-challenge/2021/biodiversity/esa_cci_rois/esa_cci_" + str(year) + "_chernobyl.tif"
@@ -113,48 +107,3 @@ def trial_esa_cci():
         # takes roughly 1 minute to run:
         raster_to_poly(path, os.path.join(os.path.dirname(os.path.realpath(__file__)),
                        "out", "esa_cci_" + str(year) + "_chernobyl.geojson"))
-
-
-def esa_cci_to_geojson(output_dir=os.path.join(os.path.dirname(os.path.realpath(__file__)), "out")):
-    for year in range(1992, 2016):
-        path = "/gws/nopw/j04/ai4er/guided-team-challenge/2021/biodiversity/esa_cci_rois/esa_cci_" + str(year) + "_chernobyl.tif"
-        raster_to_poly(path, os.path.join(output_dir, "esa_cci_" + str(year) + "_chernobyl.geojson"))
-
-
-def comp_goejsons():
-    if True:
-        year = 1995
-        old_path = "/gws/nopw/j04/ai4er/guided-team-challenge/2021/biodiversity/esa_cci_rois/esa_cci_" + str(year) + "_chernobyl.geojson"
-        df_places_old = gpd.read_file(old_path, encoding="utf-8")
-        new_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "out", "esa_cci_" + str(year) + "_chernobyl.geojson")
-        df_places_new = gpd.read_file(new_path, encoding="utf-8")
-        print(df_places_new)
-        print(df_places_old)
-
-
-"""
-(/home/users/sithom/gtc-biodiversity/env) [sithom@sci2 gtc-biodiversity]$ python3 main.py 
-Traceback (most recent call last):
-  File "fiona/_shim.pyx", line 83, in fiona._shim.gdal_open_vector
-  File "fiona/_err.pyx", line 291, in fiona._err.exc_wrap_pointer
-fiona._err.CPLE_OpenFailedError: '/gws/nopw/j04/ai4er/guided-team-challenge/2021/biodiversity/esa_cci_rois/esa_cci_1995_chernobyl.geojson' not recognized as a supported file format.
-
-During handling of the above exception, another exception occurred:
-
-Traceback (most recent call last):
-  File "main.py", line 12, in <module>
-    rtp.comp_goejsons()
-  File "/home/users/sithom/gtc-biodiversity/src/preprocessing/rast_to_poly.py", line 121, in comp_goejsons
-    df_places_old = gpd.read_file(old_path, encoding="utf-8")
-  File "/home/users/sithom/gtc-biodiversity/env/lib/python3.8/site-packages/geopandas/io/file.py", line 96, in _read_file
-    with reader(path_or_bytes, **kwargs) as features:
-  File "/home/users/sithom/gtc-biodiversity/env/lib/python3.8/site-packages/fiona/env.py", line 400, in wrapper
-    return f(*args, **kwargs)
-  File "/home/users/sithom/gtc-biodiversity/env/lib/python3.8/site-packages/fiona/__init__.py", line 256, in open
-    c = Collection(path, mode, driver=driver, encoding=encoding,
-  File "/home/users/sithom/gtc-biodiversity/env/lib/python3.8/site-packages/fiona/collection.py", line 162, in __init__
-    self.session.start(self, **kwargs)
-  File "fiona/ogrext.pyx", line 540, in fiona.ogrext.Session.start
-  File "fiona/_shim.pyx", line 90, in fiona._shim.gdal_open_vector
-fiona.errors.DriverError: '/gws/nopw/j04/ai4er/guided-team-challenge/2021/biodiversity/esa_cci_rois/esa_cci_1995_chernobyl.geojson' not recognized as a supported file format.
-"""
